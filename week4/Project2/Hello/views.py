504a75636faf90a3e2351e3087eb3052affe671d
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-05-26 10:53:02 +0100

    New year app

--- week4/Project2/Hello/views.py
+++ week4/Project2/Hello/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello(request, name):
-    return render(request, 'templates/hello/hello.html')
+    return render(request, 'hello/hello.html', {
+        "name": name.capitalize()
+    })
 
 def index(request):
     return HttpResponse("Hello, world!")
