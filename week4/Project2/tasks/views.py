ae14eb8484c0b76554884305b30ae43f3915e841
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-05-26 12:59:54 +0100

    adding some styling to tasks

--- week4/Project2/tasks/views.py
+++ week4/Project2/tasks/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+TASKS = ["foo", "bar", "baz"]
 # Create your views here.
 def index(request):
-    return render(request, 'tasks/index.html')
+    return render(request, 'tasks/index.html', {
+        "tasks" : TASKS
+    }) 
