e883375d9a100b66e17b716cfa89df9007fcfaf5
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-05-26 16:38:27 +0100

    Task app automated

--- week4/Project2/tasks/views.py
+++ week4/Project2/tasks/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
-TASKS = ["foo", "bar", "baz"]
+TASKS = ["foo", "bar", "baz", "bor"]
 # Create your views here.
 def index(request):
     return render(request, 'tasks/index.html', {
         "tasks" : TASKS
     }) 
+
+def add(request):
+    return render(request, 'tasks/add.html')
