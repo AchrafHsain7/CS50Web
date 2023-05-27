1f9d1645962dfd4eab2cdf2846f42bbc15d47037
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-05-27 18:47:35 +0100

    boiler plating

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:entry>", views.display_entry, name="display_entry")
 ]
+
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from . import util
 
 from . import util
 
@@ -6,5 +7,18 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def display_entry(request, entry):
+    entry_title = str(entry)
+    entry_data = util.get_entry(entry_title)
+    if entry_data == None:
+        return render(request, 'encyclopedia/failure.html', {
+            "error_code" : 101,
+            "error_message": "The requested entry does not exist yet"
+        })
+    return render(request, 'encyclopedia/entry.html', {
+        "title": entry_title,
+        "data" : entry_data
     })
 
