d1120c19c93a90a277612c788e60febd9d915ef6
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-05-31 21:13:16 +0100

    Adding Log In mechanisms

--- week4/airline/users/views.py
+++ week4/airline/users/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    
     return render(request, 'users/index.html')
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user != None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "users/login.html", {
+            "message" : "Invalid Log In Credentials"
+        })
+
+    return render(request, 'users/login.html')
+
+def logout_view(request):
+    pass 
+
