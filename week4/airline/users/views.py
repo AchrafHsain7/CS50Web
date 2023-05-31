3e5779053cf0824fd5ea77ace122dc89ce6c3c4a
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-05-31 21:33:20 +0100

    Final version

--- week4/airline/users/views.py
+++ week4/airline/users/views.py
@@ -8,22 +8,26 @@ def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
     
-    return render(request, 'users/index.html')
+    return render(request, 'users/user.html')
 
 def login_view(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user != None:
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
-    else:
-        return render(request, "users/login.html", {
-            "message" : "Invalid Log In Credentials"
-        })
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user != None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("user"))
+        else:
+            return render(request, "users/login.html", {
+                "message" : "Invalid Log In Credentials"
+            })
 
     return render(request, 'users/login.html')
 
 def logout_view(request):
-    pass 
+    logout(request)
+    return render(request, 'users/login.html', {
+        "message": "Logged Out"
+    })
 
