e3c65d6818243f101bc1fca11edf8a6b9007823c
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-06-01 20:26:47 +0100

    Admin app and models working as expected

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -3,11 +3,31 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-class CommentsAdmin(admin.ModelAdmin):
-    list_display = ["id"]
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "user_comments", "user_listings", "user_bids"]    
 
-admin.site.register(User) 
-admin.site.register(Comment, CommentsAdmin)  
+    @admin.display(ordering="user__comment", description="User Comments")
+    def user_comments(self, obj):
+        return obj.user_comments.all() 
+    @admin.display(ordering="user__listing", description="User Listings")
+    def user_listings(self, obj):
+        return obj.user_listings.all()
+    @admin.display(ordering="user__bid", description="User Bids")
+    def user_bids(self, obj):
+        return obj.user_bids.all() 
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ["creator", "title", "listing_bids"]   
+
+    @admin.display(ordering="listing__bid", description="Bids")
+    def listing_bids(self, obj):
+        return obj.listing_bids.all() 
+            
+
+admin.site.register(User, UserAdmin)  
+admin.site.register(Comment)   
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Bid) 
 
 
 
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -5,6 +5,27 @@ from django.db import models
 class User(AbstractUser):
      pass
 
+class Listing(models.Model):
+    creator = models.ForeignKey(User, blank=False, on_delete=models.CASCADE, related_name="user_listings") 
+    winning_user = models.ForeignKey(User, blank=False, on_delete=models.CASCADE, related_name="user_winning_listings") 
+    starting_bid = models.PositiveIntegerField(blank=False)
+    title = models.CharField(max_length=50, blank=False)
+    description = models.CharField(max_length=500)
+    image = models.URLField()
+    category = models.CharField(max_length=50) 
+
+    def __str__(self):
+        return f"{self.title}: best bid for {self.winning_user}"
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_bids")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing_bids")
+    amount = models.PositiveIntegerField(blank=False)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"Created by {self.user} with amount {self.amount}"  
+
 
 class Comment(models.Model):
     text = models.CharField(max_length=500, blank=False)
