2849c26b0151e68fa749ca06d108128ee3571003
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-06-01 19:53:21 +0100

    A mess

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ["id"]
+
+admin.site.register(User) 
+admin.site.register(Comment, CommentsAdmin)  
+
+
+
+
+
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -3,4 +3,15 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+     pass
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=500, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comments")    
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"Comment of {self.user}" 
+
+
