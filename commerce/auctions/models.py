9ef1efecec379b2a12572fe3e4bf4b58b94e5e10
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-06-02 14:35:47 +0100

    Updating the comment models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -33,10 +33,11 @@ class Bid(models.Model):
 class Comment(models.Model):
     text = models.CharField(max_length=500, blank=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comments")  
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing_comments")   
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing_comments", null=True) 
     date = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return f"Comment of {self.user}" 
+
 
 
