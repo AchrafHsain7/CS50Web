1060c0ed307f52d050036a1b3a9e22b99f03b3b9
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-06-07 18:03:06 +0100

    The receiving all email fct ready

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -6,6 +6,32 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  //when submit button is pressed in the compose form
+  document.querySelector('#compose-form').onsubmit = () => {
+        const recipients = document.querySelector('#compose-recipients').value;
+        const subject = document.querySelector('#compose-subject').value;
+        const body = document.querySelector('#compose-body').value;
+
+        fetch('/emails', {
+            method: 'POST', 
+            body: JSON.stringify({
+              recipients : recipients,
+              subject: subject,
+              body: body
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+              console.log(data);
+              load_mailbox('sent');
+        })
+        .catch(error => {
+          console.log(error);
+        })   
+
+        return false;
+  }
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -27,6 +53,25 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+
+  fetch(`emails/${mailbox}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    data.forEach(element => {
+      //more styling can be added here by creating more elements and styling them
+        const div = document.createElement('div');
+        div.style.border = 'solid';
+        div.style.margin = '5px';
+        if(element.read){
+          div.style.backgroundColor = 'grey';
+        }
+        div.innerHTML = `${element.sender}  ${element.subject}  ${element.timestamp}`;
+        document.querySelector('#emails-view').appendChild(div); 
+    });
+    
+    
+  });
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
